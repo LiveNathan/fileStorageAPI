81ab3202d443f19ff3ad65d1709ca784cbeaf2a0
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-01-27 17:41:03 -0600

    FileService class created

--- src/main/java/cnLabs/fileStorageAPI/Controllers/FileController.java
+++ src/main/java/cnLabs/fileStorageAPI/Controllers/FileController.java
@@ -1,34 +1,27 @@
 package cnLabs.fileStorageAPI.Controllers;
 
 import cnLabs.fileStorageAPI.Models.DatabaseFile;
-import cnLabs.fileStorageAPI.Repos.FileRepository;
+import cnLabs.fileStorageAPI.Services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/file-storage-api")
 public class FileController {
     @Autowired
-    FileRepository fileRepository;
+    FileService fileService;
 
     // GET file
     @GetMapping("{id}")
     public ResponseEntity<?> downloadFileById(@PathVariable Long id) {
-        boolean existsById = fileRepository.existsById(id);
-        if (existsById){
-            DatabaseFile databaseFile = fileRepository.getReferenceById(id);
+        DatabaseFile databaseFile = fileService.getFile(id);
+        if (databaseFile != null){
             return ResponseEntity.ok().contentType(MediaType.parseMediaType(databaseFile.getFileType()))
                     .header(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=\"%s\"", databaseFile.getFileName()))
                     .body(new ByteArrayResource(databaseFile.getData()));
@@ -40,16 +33,10 @@ public class FileController {
     // POST file
     @PostMapping
     public ResponseEntity<?> uploadFile(@RequestBody MultipartFile file){
-        String filename = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
         try {
-            final DatabaseFile databaseFile = DatabaseFile.builder().fileName(filename).fileType(file.getContentType()).size(file.getSize()).data(file.getBytes()).build();
-            DatabaseFile savedFile = fileRepository.save(databaseFile);  // Save file
-            savedFile.setDownloadUrl();  // Create the download URL using the returned id
-            savedFile = fileRepository.save(savedFile);  // Save again
-            return ResponseEntity.created(new URI(savedFile.getDownloadUrl())).build();
-        } catch (IOException | URISyntaxException e) {
-            // Wrap exception with custom message
-            return ResponseEntity.badRequest().body(new IllegalStateException("Well, crap. I couldn't store " + filename + " for you. Please try again and I'll do my best.", e));
+            return ResponseEntity.created(fileService.saveFile(file)).build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
---
+++ src/main/java/cnLabs/fileStorageAPI/Services/FileService.java
@@ -0,0 +1,45 @@
+package cnLabs.fileStorageAPI.Services;
+
+import cnLabs.fileStorageAPI.Models.DatabaseFile;
+import cnLabs.fileStorageAPI.Repos.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+@Service
+public class FileService {
+    @Autowired
+    FileRepository fileRepository;
+
+    public DatabaseFile getFile(Long id) {
+        if (fileRepository.existsById(id)) {
+            return fileRepository.getReferenceById(id);
+        } else {
+            return null;
+        }
+    }
+
+    public URI saveFile(MultipartFile file) throws IOException {  // How will this IOException get handled?
+        DatabaseFile savedFile = fileRepository.save(multipart2database(file));  // Save file
+        savedFile.setDownloadUrl();  // Create the download URL using the returned id
+        savedFile = fileRepository.save(savedFile);  // Save again
+        URI uri;
+        try {  // Is this the right way to handle this exception?
+            uri = new URI(savedFile.getDownloadUrl());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        return uri;
+    }
+
+    private DatabaseFile multipart2database(MultipartFile file) throws IOException {  // How will this IOException get handled?
+        String filename = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        return DatabaseFile.builder().fileName(filename).fileType(file.getContentType()).size(file.getSize()).data(file.getBytes()).build();
+    }
+}
